26b419b2f7b00e5041b9a7ef468995874c456205
WouterSioen <wouter.sioen@gmail.com>
2013-11-03 12:25:54 +0100

    Sir Trevor: use an external convertor to convert the Sir Trevor output to Html

--- backend/core/engine/form.php
+++ backend/core/engine/form.php
@@ -834,58 +834,15 @@ class BackendFormSirTrevor extends SpoonFormTextarea
 		{
 			// post/get data
 			$data = $this->getMethod(true);
-Spoon::dump($data);
+
 			// submitted by post (may be empty)
 			if(isset($data[$this->getName()]))
 			{
 				// value
 				$value = isset($data[$this->getName()]) ? $data[$this->getName()] : '';
-				$html = '';
 
-				// decode the json en loop the blocks to convert them to html
-				$json = json_decode($value);
-				foreach($json->data as $block)
-				{
-					switch($block->type)
-					{
-						case 'heading':
-							// Add Two hashes before the item. => H2
-							$text = '## ' . $block->data->text;
-							$html .= Markdown::defaultTransform($text);
-							break;
-						case 'video':
-							if($block->data->source == 'youtube')
-							{
-								// create the youtube embed link
-								$html .= '<iframe class="youtube" src="//www.youtube.com/embed/';
-								$html .= $block->data->remote_id;
-								$html .= '" frameborder="0" allowfullscreen></iframe>';
-							}
-							break;
-						case 'embedly':
-							$html .= $block->data->html;
-							break;
-						case 'quote':
-							$html .= '<blockquote>';
-							$html .= Markdown::defaultTransform($block->data->text);
-
-							// Add the cit if necessary
-							if(!empty($block->data->city))
-							{
-								$html .= '<cite>';
-								$html .= Markdown::defaultTransform($block->data->text);
-								$html .= '</city>';
-							}
-
-							$html .= '</blockquote>';
-							break;
-						default:
-							$html .= Markdown::defaultTransform($block->data->text);
-							break;
-					}
-				}
-
-				$value = $html;
+				$converter = new Converter();
+				$value = $converter->toHtml($value);
 			}
 		}
 
